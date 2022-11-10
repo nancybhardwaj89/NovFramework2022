926a332086f68ddc2bfab88ec2f474c7fc43a333
Nancy Bhardwaj <er.nancybhardwaj@gmail.com>
2022-11-10 15:10:18 +0530

    Cart branch first commit

---
+++ src/test/java/com/democart/tests/CartPageTest.java
@@ -0,0 +1,15 @@
+package com.democart.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CartPageTest {
+	
+	@Test
+	public void testCart()
+	
+	{
+		Assert.assertEquals(true, true);
+	}
+
+}
